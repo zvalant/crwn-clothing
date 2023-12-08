e3f0818e7edea6d39711e70bbd0cbfde38798e6b
Zac Valant <zac23.v@gmail.com>
2023-12-08 05:30:38 -0600

    comlete checkout page by adding increment,decrement, and clear handlers

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import React,{useContext} from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 import Button from "../button/button.component";
 import "./cart-dropdown.styles.scss";
 import CartItem from "../cart-item/cart-item.component";
@@ -6,12 +7,20 @@ import { CartContext } from '../../contexts/cart.context';
 
 const CartDropDown = () => {
   const {cartItems} = useContext(CartContext);
+  const navigate = useNavigate();
+  const handleGoToCheckout = ()=>{
+    navigate("/checkout");
+
+  }
+
   return (
     <div className = "cart-dropdown-container">
         <div className = "cart-items">
           {cartItems.map(item=><CartItem cartItem = {item}/>)}
         </div>
-        <Button>Go TO CHECKOUT</Button>
+        <Button onClick = {handleGoToCheckout}>
+          GO TO CHECKOUT
+        </Button>
         <div>
         </div>
     </div>
